86b77e6d730c377fabe49ad22f3e0b3ce9877c1c
unknown <>
2021-01-10 22:49:21 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,158 @@
+import logo from './logo.svg';
+import './App.css';
+import { Slider } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import { withStyles, makeStyles} from '@material-ui/core/styles';
+import { withUAL } from "ual-reactjs-renderer";
+
+const CustomSlider = withStyles({
+  root: {
+    color: '#52af77',
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+function App(props) {
+  const {
+    ual: { showModal, hideModal, activeUser, login, logout },
+  } = props;
+  if (activeUser) {
+    const accountName = activeUser.getAccountName();
+    accountName.then(function (result) {
+      setAccountName(result);
+    });
+  }
+  const displayaccountname = () => {
+    if (accountname) {
+      return accountname;
+    }
+  };
+  const [tokens, setTokens] = useState(1)
+  const [accountname, setAccountName] = useState("")
+
+  const logmeout = () =>{
+      logout()
+      setAccountName("")
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="maincard">
+
+          <div class="leftbar">
+            <div class="logo">
+              <img width="128px" src="https://leanfrontiers.com/wp-content/uploads/2018/12/logo-placeholder-png.png"/>
+            </div>
+            <div class="appname">
+              <a>EOSETF</a>
+            </div>
+            <div class="accountname">
+              {accountname == "" ? <a>not signed in</a> : accountname}
+            </div>
+            <div class="menuitemswrapper">
+              <table class="menuitems">
+               <tr>
+                 <td><img class="menuimg" src="assets/productbox1.svg" /></td>
+                 <td><a class="menuitemtext">Create</a></td>
+               </tr>
+               <tr>
+                 <td><img class="menuimg" src="assets/productbox2.svg" /></td>
+                 <td><a class="menuitemtext">Redeem</a></td>
+               </tr>
+               <tr>
+                 <td><img class="menuimg" src="assets/checkout.svg" /></td>
+                 <td><a class="menuitemtext">Buy</a></td>
+               </tr>
+               {accountname == "" ?
+               <tr  onClick={() => showModal()}>
+                 <td><img class="menuimg" src="assets/login.svg" /></td>
+                 <td><a class="menuitemtext">Login</a></td>
+               </tr>
+               :
+               <tr onClick={() => logmeout()}>
+                 <td><img class="menuimg" src="assets/logout.svg" /></td>
+                 <td><a class="menuitemtext">Logout</a></td>
+               </tr>
+               }
+              </table>
+            </div>
+          </div>
+
+          <div class="rightbar">
+            <div class="rightbartopbox">
+              <div class="createetftitle">
+                <a>Create EOSETF</a>
+              </div>
+              <div class="slidertext">
+                <a>You are creating {tokens} EOSETF.</a>
+              </div>
+              <div class="slider">
+                <CustomSlider
+                  defaultValue={1}
+                  aria-label="custom thumb label"
+                  step={1}
+                  min={0}
+                  max={10}
+                  onChangeCommitted={(e, val) => setTokens(val)}
+                  style={{
+                    marginBottom: "10px",
+                    "margin-top": "10px",
+                    color: "white",
+                  }}
+                />
+              </div>
+            </div>
+
+              <div class="smallcard">
+                <div class="tokenlogo">
+                  <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565"/>
+                </div>
+              </div>
+              <div class="smallcard">
+                <div class="tokenlogo">
+                  <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565"/>
+                </div>
+              </div>
+              <div class="smallcard">
+                <div class="tokenlogo">
+                  <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565"/>
+                </div>
+              </div>
+              <div class="smallcard">
+                <div class="tokenlogo">
+                  <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565"/>
+                </div>
+              </div>
+
+          </div>
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default withUAL(App);
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { UALProvider } from 'ual-reactjs-renderer'
+import { Anchor } from 'ual-anchor'
+
+const appName = "EOSETF";
+
+const chain = {
+  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+  rpcEndpoints: [
+    {
+      protocol: "https",
+      host: "dsp.eosphere.io",
+      //https://dsp.maltablock.org
+      //host: "dsp.airdropsdac.com", https://node2.blockstartdsp.com"dsp.eosphere.io",https://dsp.eosdetroit.io,https://node1.eosdsp.com
+      port: "",
+    },
+  ],
+};
+
+const anchor = new Anchor([chain], {
+  appName,
+});
+
+const supportedChains = [chain];
+const supportedAuthenticators = [
+  anchor
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UALProvider
+      chains={supportedChains}
+      authenticators={supportedAuthenticators}
+      appName={appName}
+    >
+      <App />
+    </UALProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
