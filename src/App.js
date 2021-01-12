d93c0386cebb59e1463d12fa72d6d10d5d337bb1
Vlad <vlad@Vlads-MacBook-Pro.local>
2021-01-12 21:56:43 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Slider } from '@material-ui/core';
 import React, { useState, useEffect } from "react";
-import { withStyles, makeStyles} from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { withUAL } from "ual-reactjs-renderer";
 
 const CustomSlider = withStyles({
@@ -53,10 +53,143 @@ function App(props) {
   const [tokens, setTokens] = useState(1)
   const [accountname, setAccountName] = useState("")
 
-  const logmeout = () =>{
-      logout()
-      setAccountName("")
+  const logmeout = () => {
+    logout()
+    setAccountName("")
   }
+
+
+  const send = async () => {
+    const {
+      ual: { login, displayError, showModal },
+    } = props;
+    // Via static contextType = UALContext, access to the activeUser object on this.context is now available
+    const {
+      ual: { activeUser },
+    } = props;
+
+    var efx = tokens.toFixed(4);
+
+    if (activeUser) {
+      try {
+        const transaction = {
+          actions: [
+            {
+              account: "dappservices",
+              name: "transfer",
+              authorization: [
+                {
+                  actor: displayaccountname(), // use account that was logged in
+                  permission: "active",
+                },
+              ],
+              data: {
+                from: displayaccountname(),
+                to: "consortiumtt",
+                quantity: 2.1563 * tokens + " DAPP",
+                memo: "test",
+
+              },
+            },
+
+
+            {
+              account: "effecttokens",
+              name: "transfer",
+              authorization: [
+                {
+                  actor: displayaccountname(), // use account that was logged in
+                  permission: "active",
+                },
+              ],
+              data: {
+                from: displayaccountname(),
+                to: "consortiumtt",
+                quantity: efx + " EFX",
+                memo: "test2",
+
+              },
+            },
+
+
+          ],
+        };
+        // The activeUser.signTransaction will propose the passed in transaction to the logged in Authenticator
+        await activeUser.signTransaction(transaction, {
+          broadcast: true,
+          expireSeconds: 300,
+        });
+        //alert("GREAT SUCCESS!")
+        window.location.reload(false);
+
+      } catch (error) {
+        //if (error.message.startsWith("TypeError: Cannot") == true) {
+
+      }
+    } else {
+      showModal();
+    }
+  };
+
+
+
+
+  const sendetf = async () => {
+    const {
+      ual: { login, displayError, showModal },
+    } = props;
+    // Via static contextType = UALContext, access to the activeUser object on this.context is now available
+    const {
+      ual: { activeUser },
+    } = props;
+
+
+    var eosetf = tokens.toFixed(4);
+
+
+    if (activeUser) {
+      try {
+        const transaction = {
+          actions: [
+            {
+              account: "consortiumtt",
+              name: "transfer",
+              authorization: [
+                {
+                  actor: displayaccountname(), // use account that was logged in
+                  permission: "active",
+                },
+              ],
+              data: {
+                from: displayaccountname(),
+                to: "consortiumtt",
+                quantity: eosetf + " EOSETF",
+                memo: "test",
+
+              },
+            },
+
+
+          ],
+        };
+        // The activeUser.signTransaction will propose the passed in transaction to the logged in Authenticator
+        await activeUser.signTransaction(transaction, {
+          broadcast: true,
+          expireSeconds: 300,
+        });
+        //alert("GREAT SUCCESS!")
+        window.location.reload(false);
+
+      } catch (error) {
+        //if (error.message.startsWith("TypeError: Cannot") == true) {
+
+      }
+    } else {
+      showModal();
+    }
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -64,7 +197,7 @@ function App(props) {
 
           <div class="leftbar">
             <div class="logo">
-              <img width="64px" src="assets/logo.svg"/>
+              <img width="64px" src="assets/logo.svg" />
             </div>
             <div class="appname">
               <a>EOSETF</a>
@@ -74,29 +207,29 @@ function App(props) {
             </div>
             <div class="menuitemswrapper">
               <table class="menuitems">
-               <tr>
-                 <td><img class="menuimg" src="assets/productbox1.svg" /></td>
-                 <td><a class="menuitemtext">Create</a></td>
-               </tr>
-               <tr>
-                 <td><img class="menuimg" src="assets/productbox2.svg" /></td>
-                 <td><a class="menuitemtext">Redeem</a></td>
-               </tr>
-               <tr>
-                 <td><img class="menuimg" src="assets/checkout.svg" /></td>
-                 <td><a class="menuitemtext">Buy</a></td>
-               </tr>
-               {accountname == "" ?
-               <tr  onClick={() => showModal()}>
-                 <td><img class="menuimg" src="assets/login.svg" /></td>
-                 <td><a class="menuitemtext">Login</a></td>
-               </tr>
-               :
-               <tr onClick={() => logmeout()}>
-                 <td><img class="menuimg" src="assets/logout.svg" /></td>
-                 <td><a class="menuitemtext">Logout</a></td>
-               </tr>
-               }
+                <tr onClick={() => send()}>
+                  <td><img class="menuimg" src="assets/productbox1.svg" /></td>
+                  <td><a class="menuitemtext">Create</a></td>
+                </tr>
+                <tr onClick={() => sendetf()}>
+                  <td><img class="menuimg" src="assets/productbox2.svg" /></td>
+                  <td><a class="menuitemtext">Redeem</a></td>
+                </tr>
+                <tr>
+                  <td><img class="menuimg" src="assets/checkout.svg" /></td>
+                  <td><a class="menuitemtext">Buy</a></td>
+                </tr>
+                {accountname == "" ?
+                  <tr onClick={() => showModal()}>
+                    <td><img class="menuimg" src="assets/login.svg" /></td>
+                    <td><a class="menuitemtext">Login</a></td>
+                  </tr>
+                  :
+                  <tr onClick={() => logmeout()}>
+                    <td><img class="menuimg" src="assets/logout.svg" /></td>
+                    <td><a class="menuitemtext">Logout</a></td>
+                  </tr>
+                }
               </table>
             </div>
           </div>
@@ -111,11 +244,11 @@ function App(props) {
               </div>
               <div class="slider">
                 <CustomSlider
-                  defaultValue={1}
+                  defaultValue={1.0000}
                   aria-label="custom thumb label"
-                  step={1}
+                  step={1.0000}
                   min={0}
-                  max={10}
+                  max={10.0000}
                   onChangeCommitted={(e, val) => setTokens(val)}
                   style={{
                     marginBottom: "10px",
@@ -126,57 +259,57 @@ function App(props) {
               </div>
             </div>
 
-              <div class="smallcard">
-                <div class="tokenlogo">
-                  <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565"/>
-                </div>
-                <div class="smallcardtext">
-                  <a>You have: 200 DAPP tokens</a><br/>
-                  <a>You need to send: 100 DAPP tokens</a>
-                </div>
-                <div class="trxbutton">
-                  <img class="tokenlogoimage" height="100%" src="assets/connection.svg"/>
-                </div>
+            <div class="smallcard">
+              <div class="tokenlogo">
+                <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565" />
               </div>
+              <div class="smallcardtext">
+                <a>You have: 200 DAPP tokens</a><br />
+                <a>You need to send: 100 DAPP tokens</a>
+              </div>
+              <div class="trxbutton">
+                <img class="tokenlogoimage" height="100%" src="assets/connection.svg" />
+              </div>
+            </div>
 
-              <div class="smallcard">
-                <div class="tokenlogo">
-                  <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565"/>
-                </div>
-                <div class="smallcardtext">
-                  <a>You have: 200 DAPP tokens</a><br/>
-                  <a>You need to send: 100 DAPP tokens</a>
-                </div>
-                <div class="trxbutton">
-                  <img class="tokenlogoimage" height="100%" src="assets/connection.svg"/>
-                </div>
+            <div class="smallcard">
+              <div class="tokenlogo">
+                <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565" />
               </div>
+              <div class="smallcardtext">
+                <a>You have: 200 DAPP tokens</a><br />
+                <a>You need to send: 100 DAPP tokens</a>
+              </div>
+              <div class="trxbutton">
+                <img class="tokenlogoimage" height="100%" src="assets/connection.svg" />
+              </div>
+            </div>
 
-              <div class="smallcard">
-                <div class="tokenlogo">
-                  <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565"/>
-                </div>
-                <div class="smallcardtext">
-                  <a>You have: 200 DAPP tokens</a><br/>
-                  <a>You need to send: 100 DAPP tokens</a>
-                </div>
-                <div class="trxbutton">
-                  <img class="tokenlogoimage" height="100%" src="assets/connection.svg"/>
-                </div>
+            <div class="smallcard">
+              <div class="tokenlogo">
+                <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565" />
               </div>
+              <div class="smallcardtext">
+                <a>You have: 200 DAPP tokens</a><br />
+                <a>You need to send: 100 DAPP tokens</a>
+              </div>
+              <div class="trxbutton">
+                <img class="tokenlogoimage" height="100%" src="assets/connection.svg" />
+              </div>
+            </div>
 
-              <div class="smallcard">
-                <div class="tokenlogo">
-                  <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565"/>
-                </div>
-                <div class="smallcardtext">
-                  <a>You have: 200 DAPP tokens</a><br/>
-                  <a>You need to send: 100 DAPP tokens</a>
-                </div>
-                <div class="trxbutton">
-                  <img class="tokenlogoimage" height="100%" src="assets/connection.svg"/>
-                </div>
+            <div class="smallcard">
+              <div class="tokenlogo">
+                <img class="tokenlogoimage" height="100%" src="https://assets.coingecko.com/coins/images/8116/large/dapp-logo.jpg?1554996565" />
               </div>
+              <div class="smallcardtext">
+                <a>You have: 200 DAPP tokens</a><br />
+                <a>You need to send: 100 DAPP tokens</a>
+              </div>
+              <div class="trxbutton">
+                <img class="tokenlogoimage" height="100%" src="assets/connection.svg" />
+              </div>
+            </div>
 
           </div>
 
